5e855f9a0e80ad2b2bbb29373646e581ba368fc8
gyrii84@gmail.com <gyrii84@gmail.com>
2019-08-25 16:38:51 +0300

    hw lesson 22

--- app.rb
+++ app.rb
@@ -11,8 +11,39 @@ get '/about' do
 	erb :about
 end
 
+post '/visit' do
+	@name = params[:name]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@barber = params[:barber]
+
+
+	# @message = "Dear #{@name}, we are glad that You are with us"
+
+	f = File.open "./public/user.txt", "a"
+	f.write "User: #{@name} Phone: #{@phone} Data and time: #{@datetime} Your barber will be: #{@barber} "
+
+	erb :visit
+
+
+end
+
 get '/visit' do
 	erb :visit
+end
+
+post '/contacts' do
+	@email = params[:email]
+	@message = params[:message]
+
+	# @message = "Dear #{@name}, we are glad that You are with us"
+
+	f = File.open "./public/contacts.txt", "a"
+	f.write "Mail: #{@email} Message: #{@message} "
+
+	erb :contacts
+
+
 end
 
 get '/contacts' do
